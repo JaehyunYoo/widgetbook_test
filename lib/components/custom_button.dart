eb7181c65264bb31597b52cbfade00bd68ae9f76
JaeHyun <poby@Pobyui-MacBookPro.local>
2023-10-11 13:48:47 +0900

    widgetBook test

---
+++ lib/components/custom_button.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
+
+class CustomButton extends StatelessWidget {
+  const CustomButton(
+      {super.key,
+      required this.title,
+      this.onPressed,
+      required this.textColor});
+
+  final String title;
+  final Color textColor;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Text(
+        title,
+        style: TextStyle(color: textColor),
+      ),
+    );
+  }
+}
+
+@widgetbook.UseCase(
+  name: 'Enabled',
+  type: CustomButton,
+)
+Widget enabledButton(BuildContext context) {
+  return Center(
+    child: CustomButton(
+      title: 'Enabled',
+      textColor: Colors.white,
+      onPressed: () {},
+    ),
+  );
+}
+
+@widgetbook.UseCase(
+  name: 'Disabled',
+  type: CustomButton,
+)
+Widget disabledButton(BuildContext context) {
+  return Center(
+    child: CustomButton(
+      title: 'Disabled',
+      onPressed: () {},
+      textColor: Colors.grey,
+    ),
+  );
+}
